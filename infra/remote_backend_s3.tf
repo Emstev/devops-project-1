82956e868b06fd6203b3d1f5e3641a99c9977ae7
Emstev <141884714+Emstev@users.noreply.github.com>
2025-05-29 22:58:23 +0100

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cba-proj-1-jenkins-remote-state-bucket2"
+    bucket = "cba-proj-1-jenkins-remote-state-bucket-latest1"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central"
   }
