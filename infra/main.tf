37a7fb7e368fe86372f61a7fba97fe307aa2a603
Emstev <141884714+Emstev@users.noreply.github.com>
2025-05-30 00:11:44 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -45,14 +45,14 @@ module "lb_target_group" {
 }
 
 module "alb" {
-  source                    = "./modules/load-balancer"
+  source                    = "./load-balancer"
   lb_name                   = "dev-proj-1-alb"
   is_external               = false
   lb_type                   = "application"
   sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
   subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
   tag_name                  = "dev-proj-1-alb"
-  lb_target_group_arn       = aws_lb_target_group.dev_proj_1_lb_target_group.arn
+  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
   ec2_instance_id           = module.ec2.dev_proj_1_ec2_instance_id
   lb_listner_port           = 5000
   lb_listner_protocol       = "HTTP"
