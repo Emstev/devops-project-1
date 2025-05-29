e1e74948f4f2423a4621c9969c633baff569b8b2
Emstev <141884714+Emstev@users.noreply.github.com>
2025-05-30 00:10:04 +0100

    Update main.tf

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -43,21 +43,27 @@ resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment
   port             = var.lb_target_group_attachment_port
 }
 
-data "aws_acm_certificate" "cert" {
-  domain   = "cbaflaskapp.website"
-  statuses = ["ISSUED"]
-  most_recent = true
-}
-
-resource "aws_lb_listener" "dev_proj_1_lb_https_listner" {
-  load_balancer_arn = aws_lb.dev_proj_1_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.cert.arn
+resource "aws_lb_listener" "dev_proj_1_lb_listner" {
+  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
+  port              = var.lb_listner_port
+  protocol          = var.lb_listner_protocol
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.dev_proj_1_lb_target_group.arn
+    type             = var.lb_listner_default_action
+    target_group_arn = var.lb_target_group_arn
+  }
+}
+
+# https listner on port 443
+resource "aws_lb_listener" "dev_proj_1_lb_https_listner" {
+  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
+  port              = var.lb_https_listner_port
+  protocol          = var.lb_https_listner_protocol
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
+  certificate_arn   = "arn:aws:acm:eu-central-1:324037274946:certificate/9dbac0b1-ea4e-4555-84d7-c822e27039af"
+
+  default_action {
+    type             = var.lb_listner_default_action
+    target_group_arn = var.lb_target_group_arn
   }
 }
