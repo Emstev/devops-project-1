28217662ffab78d835c6bce8175d84e169c861c4
Emstev <141884714+Emstev@users.noreply.github.com>
2025-06-02 17:51:02 +0100

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -5,7 +5,7 @@ variable "public_key" {}
 variable "subnet_id" {}
 variable "sg_enable_ssh_https" {}
 variable "enable_public_ip_address" {}
-variable "user_data_install_apache" {}
+variable "user_data_install_flaskApp.sh" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
@@ -27,11 +27,23 @@ resource "aws_instance" "dev_proj_1_ec2" {
   vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
   associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_apache
+  user_data = var.user_data_install_flaskApp.sh
 
   metadata_options {
     http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
     http_tokens   = "required" # Require the use of IMDSv2 tokens
+  }
+}
+
+resource "aws_instance" "flask_app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  ...
+
+  user_data = file("${path.module}/ec2_install_flaskApp.sh")
+
+  tags = {
+    Name = "FlaskApp"
   }
 }
 
