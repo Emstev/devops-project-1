d96b96ef4437d5b6396afc2bad19f89725a22a98
Emstev <141884714+Emstev@users.noreply.github.com>
2025-06-26 23:27:07 +0100

    Update ec2_install_flaskapp.sh

--- infra/template/ec2_install_flaskapp.sh
+++ infra/template/ec2_install_flaskapp.sh
@@ -1,13 +1,24 @@
-#! /bin/bash
-# shellcheck disable=SC2164
-cd /home/ubuntu
-yes | sudo apt update
-yes | sudo apt install python3 python3-pip
+#!/bin/bash
+set -e
+
+cd /home/ubuntu || exit
+
+apt-get update -y
+apt-get install -y python3 python3-pip git
+
+echo "[+] Cloning repo..."
 git clone https://github.com/Emstev/python-mysql-db-proj-1.git
-sleep 20
-# shellcheck disable=SC2164
-cd python-mysql-db-proj-1
-pip3 install -r requirements.txt
-echo 'Waiting for 30 seconds before running the app.py'
-setsid python3 -u app.py &
-sleep 30
+
+cd python-mysql-db-proj-1 || exit
+pip3 install --break-system-packages -r requirements.txt
+
+echo "[+] Setting up systemd service..."
+cp deployment/flaskapp.service /etc/systemd/system/flaskapp.service
+chmod 644 /etc/systemd/system/flaskapp.service
+
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable flaskapp.service
+systemctl start flaskapp.service
+
+echo "[✓] Flask app is running as a service!"
